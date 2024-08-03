0788f4482f5fae09727c19f302825510a1d57325
Zeiad Magdy <zeiadmagdy2019@gmail.com>
2024-08-03 21:34:02 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from "./products/products.component";
+import { ViewComponent } from './view/view.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LoginComponent, ProductsComponent,ViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lab3';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { ViewComponent } from './view/view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomeComponent,title:'Home'},
+    {path: 'about', component: AboutComponent,title:'About'},
+    {path: 'contact-us', component: ContactUsComponent,title:'Contact Us'},
+    {path: 'login', component: LoginComponent,title:'Login'},
+    {path: 'products', component: ProductsComponent,title:'Products'},
+    {path: 'view/:id', component: ViewComponent},
+    {path: '**', component:NotFoundComponent,title:'Page Not Found'},
+];
